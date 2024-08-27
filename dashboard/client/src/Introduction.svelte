15cc5d2bf9611faadd52472ea6af323d98b9abb1
Martin Řepa <repa.martin@protonmail.ch>
2024-08-27 15:16:21 +0200

    add more intro text and shadow box

--- dashboard/client/src/Introduction.svelte
+++ dashboard/client/src/Introduction.svelte
@@ -1,12 +1,43 @@
 <div class="d-flex justify-content-center pt-5">
-    <div class="text-center">
-        <h4>Welcome to the StratoCyberLab</h4>
-        <p></p>
-        <p class="text-secondary">
+    <div class="shadow p-3 mb-5 bg-body rounded text-center" style="max-width: 700px;">
+        <h4 class="mb-3">Welcome to the StratoCyberLab dashboard</h4>
+
+        <p class="text-secondary mb-4">
             The project to practice your cyber-security skills
         </p>
-        <p>
-            ⬅ Choose a challenge or class and happy hacking!
+
+        <p class="text-start mb-3">
+            The interface offers:
+        </p>
+
+        <ul class="text-start mb-4" style="list-style-type: decimal; padding-left: 20px;">
+            <li>Built-in terminal via SSH in a hackerlab container
+                <ul class="text-start mb-4" style="list-style-type: disc; padding-left: 20px;">
+                    <li>Click the button at the bottom of the page to open the terminal</li>
+                </ul>
+            </li>
+
+            <li class="mb-2">Class environments for remote students of <a href="https://cybersecurity.bsy.fel.cvut.cz/">Introduction to Security</a> class
+                <ul class="text-start mb-4" style="list-style-type: disc; padding-left: 20px;">
+                    <li>Choose a class in top left collapse menu and start the services</li>
+                </ul>
+            </li>
+
+            <li class="mb-2">Standalone hacking challenges in a Capture the Flag form
+                <ul class="text-start mb-4" style="list-style-type: disc; padding-left: 20px;">
+                    <li>Choose a challenge in the left menu, start it, and solve the tasks! </li>
+                </ul>
+            </li>
+
+            <li class="mb-2">Chat with the local AI assistant️</li>
+        </ul>
+
+        <p class="text-secondary mb-4">
+            The base network of your hackerlab is <i>172.20.0.0/24</i>
+        </p>
+
+        <p class="fw-bold">
+            Happy hacking!
         </p>
     </div>
 </div>
