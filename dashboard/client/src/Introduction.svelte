ff7c3598a56845dc64c5383796292b9f5789a358
Martin Řepa <repa.martin@protonmail.ch>
2024-08-30 10:05:34 +0200

    update dashboard content

--- dashboard/client/src/Introduction.svelte
+++ dashboard/client/src/Introduction.svelte
@@ -11,30 +11,32 @@
         </p>
 
         <ul class="text-start mb-4" style="list-style-type: decimal; padding-left: 20px;">
-            <li>Built-in terminal via SSH to your hackerlab machine
+            <li>Built-in terminal to your <b>hackerlab</b> machine
                 <ul class="text-start mb-4" style="list-style-type: disc; padding-left: 20px;">
                     <li>Click the button at the bottom of the page to open the terminal</li>
+                    <li>Or connect to the hackerlab directly using command <code>ssh root@172.20.0.2</code> and a password <code>ByteThem123</code></li>
                 </ul>
             </li>
 
             <li class="mb-2">Class environments for remote students of <a href="https://cybersecurity.bsy.fel.cvut.cz/">Introduction to Security</a> class
                 <ul class="text-start mb-4" style="list-style-type: disc; padding-left: 20px;">
-                    <li>Choose a class in top left collapse menu and start the services</li>
+                    <li>Choose a class in a top left collapse menu and start the services</li>
+                    <li>New environments will be released during the course. Please refresh changes using <code>git pull</code></li>
                 </ul>
             </li>
 
-            <li class="mb-2">Standalone hacking challenges in a Capture the Flag form
+            <li class="mb-2">Standalone hacking challenges in a <b>Capture the Flag</b> form
                 <ul class="text-start mb-4" style="list-style-type: disc; padding-left: 20px;">
                     <li>Choose a challenge in the left menu, start it, and solve the tasks! </li>
                 </ul>
             </li>
 
-            <li class="mb-2">Chat with the local AI assistant️</li>
+            <li class="mb-2">Optionally chat with the local AI assistant️
+                <ul class="text-start small mb-4" style="list-style-type: disc; padding-left: 20px;">
+                    <li>Note that using the chat will download a model (llama3) of size <b>~5GB</b></li>
+                </ul>
+            </li>
         </ul>
-
-        <p class="text-secondary mb-4">
-            The base network of your hackerlab is <i>172.20.0.0/24</i>
-        </p>
 
         <p class="fw-bold">
             Happy hacking!
