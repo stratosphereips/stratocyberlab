e0825b85b67c02dac2a5b456e55c8f9a6a9dfa0a
Martin Řepa <repa.martin@protonmail.ch>
2024-09-23 17:56:39 +0200

    add a "Hackerlab test" task to hello-world challenge

--- challenges/hello-world/auto-solve.sh
+++ challenges/hello-world/auto-solve.sh
@@ -8,11 +8,31 @@ then
     exit 1
 fi
 
+MATCH=`cat ~/.flag.txt | \
+       grep -o "bsy{6JmUwlxDMqAi7LGKyDifntSZuFCku7KaTbnYxkSvziYBLg4AwCjubDeBQHxE}"`
+if [[ "$MATCH" == "" ]]
+then
+    echo "Error - flag is not present in the home directory of hackerlab"
+    exit 3
+fi
+
 # submit a flag in the submission server
 RES=`curl -s 'http://172.20.0.3/api/challenges/submit' \
     -X POST \
     -H 'Content-Type: application/json' \
-    --data-binary '{"challenge_id": "hello-world", "task_id": "task2", "flag" : "bsy{simple-hello-world-flag}"}'`
+    --data-binary '{"challenge_id": "hello-world", "task_id": "task2", "flag" : "bsy{6JmUwlxDMqAi7LGKyDifntSZuFCku7KaTbnYxkSvziYBLg4AwCjubDeBQHxE}"}'`
+
+if [[ $RES != *"Congratulations"* ]]; then
+  echo "Failed to submit the flag - $RES"
+  exit 4
+fi
+
+
+# submit a flag in the submission server
+RES=`curl -s 'http://172.20.0.3/api/challenges/submit' \
+    -X POST \
+    -H 'Content-Type: application/json' \
+    --data-binary '{"challenge_id": "hello-world", "task_id": "task3", "flag" : "bsy{simple-hello-world-flag}"}'`
 if [[ $RES != *"Congratulations"* ]]; then
   echo "Failed to submit the flag - $RES"
   exit 5
