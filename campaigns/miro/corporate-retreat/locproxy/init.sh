ebb1859095b28ee4b838e75b66cbc0353a6dc882
Miroslav Holeček <miroslav@holecekm.cz>
2025-01-06 20:51:26 +0100

    miro/corporate-retreat: add means to access locproxy

--- campaigns/miro/corporate-retreat/locproxy/init.sh
+++ campaigns/miro/corporate-retreat/locproxy/init.sh
@@ -5,6 +5,9 @@ echo "Configuring routing..."
 ip route del default
 ip route add default via 10.0.0.4
 
+echo "Starting SSHd"
+/usr/sbin/sshd
+
 echo "Starting proxy"
 
 mitmdump --set confdir=/app -w "/var/log/proxy/web-proxy-$(date --iso-8601).log"
