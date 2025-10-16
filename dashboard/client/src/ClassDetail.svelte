183a148bbd611e67622775bd4f45023b058d6286
Martin Řepa <repa.martin@protonmail.ch>
2025-10-16 14:35:34 +0200

    fix livestream YT link

--- dashboard/client/src/ClassDetail.svelte
+++ dashboard/client/src/ClassDetail.svelte
@@ -11,7 +11,7 @@
 
   // Playlist with all livestreams
   // https://www.youtube.com/playlist?list=PLQL6z4JeTTQmu09ItEQaqjt6tk0KnRsLh
-  const liveStreamUrl = 'https://www.youtube.com/embed/5_XcD18o08Y?list=PLQL6z4JeTTQmu09ItEQaqjt6tk0KnRsLh'
+  const liveStreamUrl = 'https://www.youtube.com/embed/nK6WGeyVEz8'
 
   const LECTURE_DURATION_MS = 200 * 60 * 1000; // 200 minutes
   const BUFFER_BEFORE_MS = 10 * 60 * 1000; // 10 minutes
