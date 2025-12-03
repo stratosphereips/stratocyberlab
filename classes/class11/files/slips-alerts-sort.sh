90faa1215e549972a4d1c7fca9b6a1164b408a3f
Veronica Valeros <2458879+verovaleros@users.noreply.github.com>
2025-12-03 14:14:37 +0100

    Update to accept alert log as a parameter

--- classes/class11/files/slips-alerts-sort.sh
+++ classes/class11/files/slips-alerts-sort.sh
@@ -1,1 +1,12 @@
-cat alerts.log |awk -F 'Detected' '{print $2}'|sort|uniq -c|sort -rn
+#!/bin/bash
+
+# If an argument is provided, use it.
+# Otherwise default to ./alerts.log
+ALERTS_LOG="${1:-alerts.log}"
+
+cat "$ALERTS_LOG" \
+  | awk -F 'Detected' '{print $2}' \
+  | grep "threat level: high" \
+  | sort \
+  | uniq -c \
+  | sort -rn
