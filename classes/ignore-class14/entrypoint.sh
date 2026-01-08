2c8f8127891b21aa9e61c7a94f290b7528abc0e6
Ondrej Lukas <ondrej.lukas95@gmail.com>
2026-01-08 13:41:36 +0100

    cleanup after renaming

--- classes/ignore-class14/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start rsyslog
-echo "Starting rsyslog..."
-rsyslogd
-
-# Start sshd
-echo "Starting sshd..."
-/usr/sbin/sshd -D
--- classes/ignore-class14/ssh-server/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start rsyslog
-echo "Starting rsyslog..."
-rsyslogd
-
-# Start SSH server
-echo "Starting sshd..."
-/usr/sbin/sshd -D
